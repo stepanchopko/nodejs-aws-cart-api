d4a1c093d2f15e0889aea5cbde62007af226ce80
Stepan Chopko <61800263+stepanchopko@users.noreply.github.com>
2025-11-01 16:00:41 +0100

    Configured RDS

--- infra/lib/cart-api-stack.ts
+++ infra/lib/cart-api-stack.ts
@@ -2,12 +2,62 @@ import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { aws_apigateway as apigateway } from 'aws-cdk-lib';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class CartApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'CartApiVpc', {
+      maxAzs: 2,
+    });
+
+    const rdsSecurityGroup = new ec2.SecurityGroup(this, 'RdsSecurityGroup', {
+      vpc,
+      description: 'Allow Lambda access to RDS PostgreSQL',
+      allowAllOutbound: true,
+    });
+
+    const lambdaSecurityGroup = new ec2.SecurityGroup(
+      this,
+      'LambdaSecurityGroup',
+      {
+        vpc,
+        description: 'Lambda security group',
+        allowAllOutbound: true,
+      },
+    );
+
+    rdsSecurityGroup.addIngressRule(
+      lambdaSecurityGroup,
+      ec2.Port.tcp(5432),
+      'Allow Lambda security group access to PostgreSQL',
+    );
+
+    const dbInstance = new rds.DatabaseInstance(this, 'CartApiPostgres', {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_14,
+      }),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      multiAz: false,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      securityGroups: [rdsSecurityGroup],
+      publiclyAccessible: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deleteAutomatedBackups: true,
+    });
 
     const lambdaFunction = new NodejsFunction(this, 'LambdaFunction', {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -15,6 +65,13 @@ export class CartApiStack extends cdk.Stack {
       projectRoot: path.join(__dirname, '../..'),
       timeout: cdk.Duration.seconds(30),
       memorySize: 1024,
+      vpc,
+      securityGroups: [lambdaSecurityGroup],
+      environment: {
+        DB_SECRET_ARN: dbInstance.secret?.secretArn || '',
+        DB_HOST: dbInstance.dbInstanceEndpointAddress,
+        DB_PORT: dbInstance.dbInstanceEndpointPort,
+      },
       bundling: {
         forceDockerBundling: false,
         externalModules: [
@@ -26,6 +83,8 @@ export class CartApiStack extends cdk.Stack {
         ],
       },
     });
+
+    dbInstance.secret?.grantRead(lambdaFunction);
 
     const api = new apigateway.RestApi(this, 'NestApi', {
       restApiName: 'Nest Service',
